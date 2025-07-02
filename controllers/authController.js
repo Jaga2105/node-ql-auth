3514c896f38a7420043a4afb9ab5a33fbbf44354
Jagannath Nayak <jagannathnayak2105@gmail.com>
2025-07-02 17:39:57 +0530

    some fixes

--- controllers/authController.js
+++ controllers/authController.js
@@ -69,29 +69,3 @@ module.exports = {
   },
 };
 
-// authenticate: async (req, res, next) =>{
-//   const authHeader = req.headers.authorization;
-
-//   if(authHeader){
-//     const token = authHeader.split(" ")[1];
-//     if(!token){
-//       return res.status(400).json({
-//         error: "No token found!"
-//       })
-//     }
-
-//     jwt.verify(token, process.env.JWT_SECRET, (err, user)=>{
-//       if(err){
-//         return res.status(403).json({
-//           error: "Token not valid!"
-//         })
-//       }
-//       req.user= user;
-//       next();
-//     })
-//   }else{
-//     return res.status(401).json({
-//       error: "Unauthorized! No token found"
-//     })
-//   }
-// }
